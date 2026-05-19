18229ce1f5f1a152f175394b48100ba8b7f7f611
Jamie Fairweather <jamiefairweather2000@gmail.com>
2026-05-19 20:24:13 +0100

    setup coss ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,14 @@
 import type { Metadata } from 'next'
 import './globals.css'
+import { Geist, Inter, Geist_Mono } from 'next/font/google'
+import { cn } from '@/lib/utils'
+import { ThemeProvider } from '@/components/theme-provider'
+
+const geistMono = Geist_Mono({ subsets: ['latin'], variable: '--font-mono' })
+
+const interHeading = Inter({ subsets: ['latin'], variable: '--font-heading' })
+
+const inter = Inter({ subsets: ['latin'], variable: '--font-sans' })
 
 export const metadata: Metadata = {
     title: 'diagram-app',
@@ -12,8 +21,12 @@ export default function RootLayout({
     children: React.ReactNode
 }>) {
     return (
-        <html lang="en">
-            <body>{children}</body>
+        <html lang="en" className={cn('font-sans', 'font-mono', inter.variable, interHeading.variable, geistMono.variable)} suppressHydrationWarning>
+            <body className="min-h-screen min-w-screen bg-background">
+                <ThemeProvider attribute="class" defaultTheme="dark" forcedTheme="dark" disableTransitionOnChange>
+                    {children}
+                </ThemeProvider>
+            </body>
         </html>
     )
 }
